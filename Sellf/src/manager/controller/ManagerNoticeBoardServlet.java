8ba749e7bbd115a776f911f840ba6bc9faac1323
김준석 <ggoz119@naver.com>
2018-06-22 04:13:48 +0900

    Revert "kjs_back단"
    This reverts commit b2d95e0b625cc6d1f1a4252dae6783ed0c92beb6.

--- Sellf/src/manager/controller/ManagerNoticeBoardServlet.java
+++ Sellf/src/manager/controller/ManagerNoticeBoardServlet.java
@@ -3,14 +3,12 @@ package manager.controller;
 import java.io.IOException;
 
 import javax.servlet.RequestDispatcher;
-import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import common.JDBCTemplate;
 import manager.model.service.ManagerBoardService;
 import manager.model.vo.ManagerBoardPageData;
 
@@ -33,10 +31,6 @@ public class ManagerNoticeBoardServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		ServletContext context = getServletContext();
-		String fullPath = context.getRealPath("/WEB-INF/property/driver.properties");
-		JDBCTemplate.setDriverPath(fullPath);
-		
 		int currentPage ;//���� ���������� �����ϴ� ����
 		if(request.getParameter("currentPage")==null) 
 		{
